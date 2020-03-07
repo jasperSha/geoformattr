e5fa48ad48e36b93eca21ce9fa117bcbea129b9f
jasperSha <jaysha54@gmail.com>
2020-03-06 16:23:01 -0800

    clean and rebuild

--- R/matchColClasses.R
+++ R/matchColClasses.R
@@ -8,7 +8,7 @@
 #'@param dataframe
 #'@keywords match, mutate, classes, class, dataframe
 #'@example matchColClasses(df1, df2)
-#'@return second dataframe
+#'@return dataframe
 #'@name matchColClasses
 #'@export
 #'
@@ -16,13 +16,12 @@
 matchColClasses <- function(df1, df2) {
   sharedColNames <- names(df1)[names(df1) %in% names(df2)]
   sharedColTypes <- sapply(df1[,sharedColNames], class)
-  
+
   for (n in sharedColNames) {
     class(df2[, n]) <- sharedColTypes[n]
   }
-  
+
   return(df2)
-  
+
 }
 
-devtools::document()
